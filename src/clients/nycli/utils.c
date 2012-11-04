xmms2/xmms2-devel

commit 39d6287faa93a62d7ec9bdfc05c495eb3de82a80

OTHER: Don't jump when no media is added.

src/clients/nycli/utils.c
@@ -1315,48 +1315,35 @@ add_pls (xmmsc_result_t *plsres, cli_infos_t *infos,
 	xmmsc_result_unref (plsres);
 }
 
+/**
+ * Add a list of ids to a playlist, starting at a given
+ * position.
+ */
 void
-add_list (xmmsc_result_t *matching, cli_infos_t *infos,
+add_list (xmmsv_t *idlist, cli_infos_t *infos,
           const gchar *playlist, gint pos)
 
 {
 	/* FIXME: w00t at code copy-paste, please modularize */
-	xmmsc_result_t *insres;
 	gint32 id;
-	gint offset;
-	const gchar *err;
 
-	xmmsv_t *val;
+	xmmsv_list_iter_t *it;
+	xmmsv_get_list_iter (idlist, &it);
 
-	val = xmmsc_result_get_value (matching);
+	while (xmmsv_list_iter_valid (it)) {
+		xmmsv_t *entry;
+		xmmsv_list_iter_entry (it, &entry);
 
-	offset = 0;
+		if (xmmsv_get_int (entry, &id)) {
+			xmmsc_result_t *res;
+			res = xmmsc_playlist_insert_id (infos->sync, playlist, pos++, id);
 
-	if (xmmsv_get_error (val, &err) || !xmmsv_is_type (val, XMMSV_TYPE_LIST)) {
-		g_printf (_("Error retrieving the media matching the pattern!\n"));
-	} else {
-		xmmsv_list_iter_t *it;
-
-		xmmsv_get_list_iter (val, &it);
-
-		/* Loop on the matched media */
-		for (xmmsv_list_iter_first (it);
-		     xmmsv_list_iter_valid (it);
-		     xmmsv_list_iter_next (it)) {
-			xmmsv_t *entry;
-
-			xmmsv_list_iter_entry (it, &entry);
-
-			if (xmmsv_get_int (entry, &id)) {
-				insres = xmmsc_playlist_insert_id (infos->sync, playlist,
-				                                   pos + offset, id);
-				xmmsc_result_wait (insres);
-				xmmsc_result_unref (insres);
-				offset++;
-			}
+			xmmsc_result_wait (res);
+			xmmsc_result_unref (res);
 		}
+
+		xmmsv_list_iter_next (it);
 	}
-	xmmsc_result_unref (matching);
 }
 
 void
@@ -1911,33 +1898,38 @@ decode_url (const gchar *string)
 }
 
 /* Transform a path (possibly absolute or relative) into a valid XMMS2
- * path with protocol prefix. The resulting string must be freed
- * manually.
+ * path with protocol prefix, and applies a file test to it.
+ * The resulting string must be freed manually.
+ *
+ * @return the path in URL format if the test passes, or NULL.
  */
 gchar *
 format_url (const gchar *path, GFileTest test)
 {
 	gchar rpath[XMMS_PATH_MAX];
 	const gchar *p;
-	gchar *url;
 
 	/* Check if path matches "^[a-z]+://" */
 	for (p = path; *p >= 'a' && *p <= 'z'; ++p);
-	if (*p == ':' && *(++p) == '/' && *(++p) == '/') {
-		url = g_strdup (path);
-	} else {
-		/* OK, so this is NOT an valid URL */
 
+	/* If this test passes, path is a valid url and
+	 * p points past its "file://" prefix.
+	 */
+	if (!(*p == ':' && *(++p) == '/' && *(++p) == '/')) {
+
+		/* This is not a valid URL, try to work with
+		 * the absolute path.
+		 */
 		if (!x_realpath (path, rpath)) {
 			return NULL;
 		}
 
-		if (!g_file_test (rpath, test)) {
-			return NULL;
-		}
-
-		url = g_strconcat ("file://", rpath, NULL);
+		p = rpath;
 	}
 
-	return x_path2url (url);
+	if (!g_file_test (p, test)) {
+		return NULL;
+	}
+
+	return g_strconcat ("file://", p, NULL);
 }

src/clients/nycli/commands.c
@@ -1137,13 +1137,15 @@ cli_add_dir (cli_infos_t *infos, const gchar *url,
  *
  * Process and add file arguments to a playlist at a given position.
  * The files may be regular or playlist files.
+ *
+ * @return whether media has been added to the playlist.
  */
-static void
+static gboolean
 cli_add_fileargs (cli_infos_t *infos, command_context_t *ctx,
                   const gchar *playlist, gint pos)
 {
 	gint i;
-	gboolean plsfile, norecurs;
+	gboolean plsfile, norecurs, ret = FALSE;
 	xmmsv_t *attributes;
 
 	command_flag_boolean_get (ctx, "pls", &plsfile);
@@ -1190,6 +1192,8 @@ cli_add_fileargs (cli_infos_t *infos, command_context_t *ctx,
 
 			pos++; /* next insert at next pos, to keep order */
 			browse_entry_free (entry);
+
+			ret = TRUE;
 		}
 
 		g_free (encoded);
@@ -1198,15 +1202,17 @@ cli_add_fileargs (cli_infos_t *infos, command_context_t *ctx,
 	}
 
 	xmmsv_unref (attributes);
-	return;
+	return ret;
 }
 
 /**
  * Helper function for cli_add.
  *
  * Process and add pattern arguments to a playlist at a given position.
+ *
+ * @return Whether any media has been added to the playlist.
  */
-static void
+static gboolean
 cli_add_pattern (cli_infos_t *infos, command_context_t *ctx,
                  const gchar *playlist, gint pos)
 {
@@ -1214,6 +1220,7 @@ cli_add_pattern (cli_infos_t *infos, command_context_t *ctx,
 	gchar *pattern = NULL;
 	xmmsv_t *order = NULL;
 	xmmsv_coll_t *query;
+	gboolean ret = FALSE;;
 
 	command_arg_longstring_get_escaped (ctx, 0, &pattern);
 
@@ -1222,6 +1229,7 @@ cli_add_pattern (cli_infos_t *infos, command_context_t *ctx,
 	} else if (!xmmsc_coll_parse (pattern, &query)) {
 		g_printf (_("Error: failed to parse the pattern!\n"));
 	} else {
+		xmmsv_t *idlist;
 		xmmsc_result_t *res;
 
 		if (command_flag_stringlist_get (ctx, "order", &sortby)) {
@@ -1231,14 +1239,25 @@ cli_add_pattern (cli_infos_t *infos, command_context_t *ctx,
 
 		res = xmmsc_coll_query_ids (infos->sync, query, order, 0, 0);
 		xmmsc_result_wait (res);
-		add_list (res, infos, playlist, pos);
+		idlist = xmmsc_result_get_value (res);
+
+		if (!xmmsv_is_type (idlist, XMMSV_TYPE_LIST)) {
+			g_printf (_("Error retrieving the media matching the pattern!\n"));
+		} else if (!xmmsv_list_get_size (idlist)) {
+			g_printf (_("Warning: pattern didn't resolve to any media.\n"));
+		} else {
+			add_list (idlist, infos, playlist, pos);
+			ret = TRUE;
+		}
+
 		xmmsc_coll_unref (query);
+		xmmsc_result_unref (res);
 	}
 
 	g_free (pattern);
 	if (order) xmmsv_unref (order);
 
-	return;
+	return ret;
 }
 
 gboolean
@@ -1246,7 +1265,7 @@ cli_add (cli_infos_t *infos, command_context_t *ctx)
 {
 	gint pos, i;
 	const gchar *playlist;
-	gboolean forceptrn, plsfile, fileargs, jump;
+	gboolean forceptrn, plsfile, fileargs, jump, added;
 
 	/*
 	--file  Add a path from the local filesystem
@@ -1289,7 +1308,7 @@ cli_add (cli_infos_t *infos, command_context_t *ctx)
 	}
 
 	if (fileargs) {
-		cli_add_fileargs (infos, ctx, playlist, pos);
+		added = cli_add_fileargs (infos, ctx, playlist, pos);
 	} else {
 		gboolean norecurs;
 		xmmsv_t *attributes;
@@ -1308,10 +1327,10 @@ cli_add (cli_infos_t *infos, command_context_t *ctx)
 
 		xmmsv_unref (attributes);
 
-		cli_add_pattern (infos, ctx, playlist, pos);
+		added = cli_add_pattern (infos, ctx, playlist, pos);
 	}
 
-	if (jump) {
+	if (added && jump) {
 		xmmsc_result_t *res;
 
 		res = xmmsc_playlist_set_next (infos->sync, pos);

src/clients/nycli/utils.h
@@ -60,7 +60,7 @@ void playback_pause (cli_infos_t *infos);
 void playback_toggle (cli_infos_t *infos);
 void set_next_rel (cli_infos_t *infos, gint offset);
 void add_pls (xmmsc_result_t *plsres, cli_infos_t *infos, const gchar *playlist, gint pos);
-void add_list (xmmsc_result_t *matching, cli_infos_t *infos, const gchar *playlist, gint pos);
+void add_list (xmmsv_t *matching, cli_infos_t *infos, const gchar *playlist, gint pos);
 void add_recursive (cli_infos_t *infos, const gchar *playlist, const gchar *path, gint pos, gboolean norecurs);
 void move_entries (xmmsc_result_t *matching, cli_infos_t *infos, const gchar *playlist, gint pos);
 void remove_cached_list (xmmsc_result_t *matching, cli_infos_t *infos);

chrippa/xmms2

commit f43062d3b44b19f21284601f9435060fe44bd075

OTHER: Fix regression in nycli's add command.
39d6287faa93a62d7ec9bdfc05c495eb3de82a80 broke
nycli's ability to add non-file URLs.

src/clients/nycli/utils.c
@@ -1908,6 +1908,7 @@ format_url (const gchar *path, GFileTest test)
 {
 	gchar rpath[XMMS_PATH_MAX];
 	const gchar *p;
+	gchar *url;
 
 	/* Check if path matches "^[a-z]+://" */
 	for (p = path; *p >= 'a' && *p <= 'z'; ++p);
@@ -1924,12 +1925,14 @@ format_url (const gchar *path, GFileTest test)
 			return NULL;
 		}
 
-		p = rpath;
+		if (!g_file_test (rpath, test)) {
+			return NULL;
+		}
+
+		url = g_strconcat ("file://", rpath, NULL);
+	} else {
+		url = g_strdup (path);
 	}
 
-	if (!g_file_test (p, test)) {
-		return NULL;
-	}
-
-	return g_strconcat ("file://", p, NULL);
+	return x_path2url (url);
 }